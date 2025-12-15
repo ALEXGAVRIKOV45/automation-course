8dd3b8fc7e3f67dc78056a92b38b3ca8fe408235
Alex Gavrikov <gavrikov@lanit.ru>
2025-12-15 14:13:10 +0500

    Добавлены автотесты

---
+++ src/test/java/auto/HW1StatusCodeInterceptionTest.java
@@ -0,0 +1,59 @@
+package auto;
+
+import base.BaseTest;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Route;
+import com.microsoft.playwright.assertions.LocatorAssertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Collections;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class HW1StatusCodeInterceptionTest extends BaseTest {
+
+    String btn404 = "//a[@href='status_codes/404']";
+    String newText = "//h3[text()='Mocked Success Response']";
+
+    @Test
+    void testMockedStatusCode() {
+        // Перехват запроса к /status_codes/404
+        context.route("**/status_codes/404", route -> {
+            route.fulfill(new Route.FulfillOptions()
+                    .setStatus(200)
+                    .setHeaders(Collections.singletonMap("Content-Type", "text/html"))
+                    .setBody("<h3>Mocked Success Response</h3>")
+            );
+        });
+
+        page.onRequest(request -> {
+            if (request.url().equals("https://the-internet.herokuapp.com/status_codes/404")){
+                System.out.println("Request URL: " + request.url());
+                System.out.println("Request Method: " + request.method());
+                System.out.println("Is Navigation Request: " + request.isNavigationRequest());
+            }
+        });
+        page.onResponse(response -> {
+            if (response.url().equals("https://the-internet.herokuapp.com/status_codes/404")) {
+                System.out.println("Response URL: " + response.url());
+                System.out.println("Response Status: " + response.status());
+                Assertions.assertEquals(200, response.status(), "Статус страницы должен быть 200");
+            }
+        });
+
+        page.navigate("https://the-internet.herokuapp.com/status_codes");
+
+        // Клик по ссылке "404"
+        Locator button404 = page.locator(btn404);
+        assertThat(button404).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(3000));
+        button404.click();
+
+        // Проверка мок-текста
+        Locator newText404 = page.locator(newText);
+        assertThat(newText404).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(3000));
+
+
+    }
+
+
+}
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,18 +2,23 @@ package base;
 
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Collections;
 
 public class BaseTest {
     Playwright playwright;
     Browser browser;
-    BrowserContext context;
+    public BrowserContext context;
     public Page page;
+
 
     @BeforeEach
     void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                .setHeadless(false));
         context = browser.newContext();
         page = context.newPage();
     }
@@ -22,6 +27,7 @@ public class BaseTest {
 
     @AfterEach
     void tearDown() {
+        browser.close();
         playwright.close();
     }
 }
