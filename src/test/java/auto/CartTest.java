3b0e81f2ce849acb37d5453380e027cbf6f4fd5c
Alex Gavrikov <gavrikov@lanit.ru>
2026-01-01 21:07:52 +0500

    Практическое задание №4

--- src/test/java/auto/CartTest.java
+++ src/test/java/auto/CartTest.java
@@ -2,7 +2,13 @@ package auto;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.assertions.LocatorAssertions;
+import io.qameta.allure.Allure;
+
+import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,6 +24,7 @@ public class CartTest {
     Browser browser;
     private BrowserContext context;
     private Page page;
+    private Path screenshotDir;
 
 
     @BeforeEach
@@ -26,51 +33,91 @@ public class CartTest {
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
                 .setSlowMo(1000)
                 .setHeadless(false));
+        screenshotDir = Paths.get("screenshorts/");
         context = browser.newContext(new Browser.NewContextOptions()
                 .setRecordVideoDir(Paths.get("videos/")));
+
         page = context.newPage();
         page.setViewportSize(1920, 1080);
     }
 
+    @RegisterExtension
+    TestWatcher watcher = new TestWatcher() {
+        @Override
+        public void testFailed(ExtensionContext extensionContext, Throwable cause) {
+
+            try {
+                if (page != null && !page.isClosed()) {
+                    String testName = extensionContext.getDisplayName();
+                    Path screenshotPath = screenshotDir.resolve(getTimestampPath(testName) + "_test_FAIL.png");
+
+                    byte[] screenshot = page.screenshot(new Page.ScreenshotOptions()
+                            .setPath(screenshotPath)
+                            .setFullPage(true));
+                    saveScreenshotToAllure(screenshot, testName);
+                    System.out.println("Скриншот при падении сохранен: " + screenshotPath);
+                }
+
+            } catch (Exception e) {
+                System.err.println("Ошибка при создании скриншота: " + e.getMessage());
+            }
+        }
+    };
+
+    @Attachment(value = "Скриншот при падении: {name}", type = "image/png")
+    private byte[] saveScreenshotToAllure(byte[] screenshot, String name) {
+        return screenshot;
+    }
+
     @Test
+    @DisplayName("Test add,del cart")
     void testCartActions() {
-        page.navigate("https://www.saucedemo.com/");
-        assertThat(page.locator("input[id='login-button']")).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(3000));
+        Allure.step("1. Открытие сайта", () -> {
+            page.navigate("https://www.saucedemo.com/");
+            assertThat(page.locator("input[id='login-button']")).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(3000));
+        });
 
-        Locator userName = page.locator("input[id='user-name']");
-        Locator passWord = page.locator("input[id='password']");
-        userName.fill("standard_user");
-        passWord.fill("secret_sauce");
 
-        page.locator("input[id='login-button']").click();
-        assertThat(page.locator("a[data-test='shopping-cart-link']")).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(3000));
+        Allure.step("2. Заполняем учетные данные", () -> {
+            Locator userName = page.locator("input[id='user-name']");
+            Locator passWord = page.locator("input[id='password']");
+            userName.fill("standard_user");
+            passWord.fill("secret_sauce");
+        });
 
+        Allure.step("3. Клик по кнопки входа", () -> {
+            page.locator("input[id='login-button']").click();
+            assertThat(page.locator("a[data-test='shopping-cart-link']")).isVisible(new LocatorAssertions.IsVisibleOptions().setTimeout(3000));
+        });
 
         String cart = "a[data-test='shopping-cart-link']";
-        // Добавление товара 1
-        page.click("button[id='add-to-cart-sauce-labs-bolt-t-shirt']");
-        page.locator(cart).screenshot(new Locator.ScreenshotOptions()
-                .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_add.png"))));
+        Allure.step("4. Добавление товара 1", () -> {
+            // Добавление товара 1
+            page.click("button[id='add-to-cart-sauce-labs-bolt-t-shirt']");
+            page.locator(cart).screenshot(new Locator.ScreenshotOptions()
+                    .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_add.png"))));
+            page.waitForTimeout(2000);
+        });
 
+        Allure.step("5. Добавление товара 2", () -> {
+            // Добавление товара 2
+            page.click("button[id='add-to-cart-sauce-labs-bike-light']");
+            page.locator(cart).screenshot(new Locator.ScreenshotOptions()
+                    .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_add.png"))));
+        });
+        Allure.step("6. Удаление товара 1", () -> {
+            // Удаление товара 1
+            page.click("button[id='remove-sauce-labs-bolt-t-shirt']");
+            page.locator(cart).screenshot(new Locator.ScreenshotOptions()
+                    .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_remove.png"))));
+        });
         page.waitForTimeout(2000);
-
-        // Добавление товара 2
-        page.click("button[id='add-to-cart-sauce-labs-bike-light']");
-        page.locator(cart).screenshot(new Locator.ScreenshotOptions()
-                .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_add.png"))));
-
-        // Удаление товара 1
-        page.click("button[id='remove-sauce-labs-bolt-t-shirt']");
-        page.locator(cart).screenshot(new Locator.ScreenshotOptions()
-                .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_remove.png"))));
-
-        page.waitForTimeout(2000);
-
-        // Удаление товара 2
-        page.click("button[id='remove-sauce-labs-bike-light']");
-        page.locator(cart).screenshot(new Locator.ScreenshotOptions()
-                .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_remove.png"))));
-
+        Allure.step("7. Удаление товара 2", () -> {
+            // Удаление товара 2
+            page.click("button[id='remove-sauce-labs-bike-light']");
+            page.locator(cart).screenshot(new Locator.ScreenshotOptions()
+                    .setPath(Paths.get("screenshorts/" + getTimestampPath("cart_after_remove.png"))));
+        });
     }
 
     private Path getTimestampPath(String filename) {
@@ -78,6 +125,20 @@ public class CartTest {
         return Paths.get(now.format(DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss")) + filename);
     }
 
+//    @AfterEach
+//    void attachScreenshotOnFailure(ExtensionContext extensionContext) {
+//        // Проверяем наличие исключения
+//        if (extensionContext.getExecutionException().isPresent()) {
+//            byte[] screenshot = page.screenshot();
+//            Allure.addAttachment(
+//                    "Screenshot on Failure",
+//                    "image/png",
+//                    new ByteArrayInputStream(screenshot),
+//                    ".png"
+//            );
+//        }
+//        context.close();
+//    }
     @AfterEach
     void teardown() {
         context.close();
@@ -91,7 +152,6 @@ public class CartTest {
         browser.close();
         playwright.close();
     }
-
-
 }
+
 
