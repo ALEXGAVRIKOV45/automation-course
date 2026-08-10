f12c0f6b44665cd696e4e5745fc04e09b7d08268
Alex Gavrikov <gavrikov@lanit.ru>
2026-08-10 17:03:54 +0500

    Практическое задание №13

---
+++ src/test/java/auto/MobileDynamicControlsTest.java
@@ -0,0 +1,58 @@
+package auto;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class MobileDynamicControlsTest {
+    Playwright playwright;
+    Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeEach
+    void setUp() {
+        playwright = Playwright.create();
+
+        // Настройка параметров iPad Pro 11
+        Browser.NewContextOptions deviceOptions = new Browser.NewContextOptions()
+                .setUserAgent("Mozilla/5.0 (iPad; CPU OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)")
+                .setViewportSize(834, 1194)
+                .setDeviceScaleFactor(2)
+                .setIsMobile(true)
+                .setHasTouch(true);
+
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); // Установите headless на false для визуализации
+        context = browser.newContext(deviceOptions);
+        page = context.newPage();
+    }
+
+    @Test
+    void testInputEnabling() {
+        page.navigate("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //Ваш код...
+        Locator buttonEnable = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Enable"));
+        Locator textboxField = page.getByRole(AriaRole.TEXTBOX);
+
+        buttonEnable.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+        buttonEnable.click();
+
+        buttonEnable.waitFor(new Locator.WaitForOptions().setTimeout(30000));
+        assertThat(buttonEnable).isHidden();
+
+        assertThat(textboxField).isEditable();
+    }
+
+    @AfterEach
+    void tearDown() {
+        context.close();
+        browser.close();
+        playwright.close();
+    }
+}
